e2bf8b8b11d0493c31f1c44590eb6fd826e5ecf0
Simone Peressini <86060598+s1mon-perez@users.noreply.github.com>
2022-02-09 20:23:57 +0000

    class CSVTIMESERIES avg monthly difference

--- esame.py
+++ esame.py
@@ -1,115 +1,101 @@
-# coding=utf-8
-from datetime import datetime
-info = []
-lista_date = []
-passengers = []
-# ====================================================
-#           DEFINIZIONE CLASSE ERRORI
-# ====================================================
+import datetime
 
+# ======================
+# CLASSE ERRORI
+# ======================
 class ExamException(Exception):
     pass
 
-# ====================================================
-#           DEFINIZIONE CLASSE CSV E SUO METODO
-# ====================================================
-
-class CSVTimeSeriesFile:
+# ========================= #
+#  CSVTIMESERIESFILE CLASS  #
+# ========================= #
+class CSVTimeSeriesFile():
     def __init__(self, name):
         self.name = name
-        self.can_read = True
+        if not isinstance(name,str):
+            print("ATTENZIONE file non in formato stringa, si prova a convertirlo")
+            try:
+                self.name = str(name)
+            except Exception:
+                raise ExamException("Non è stato possibile convertire il file in stringa")
+
+    def get_data(self):
+        lista_finale = []
+        liste_timestamp = []
         try:
             my_file = open(self.name, 'r')
-            my_file.readline()
-        except Exception as e:
-            self.can_read = False
-            print('Errore in apertura del file: "{}"'.format(e))
-
-    # definizione metodo get_data
-    def get_data(self):
-        # se non posso aprire il file ritorno errore ed esco
-        if not self.can_read:
-            print('Errore, file non aperto o illeggibile')
-            return None
-        else:
-            # apro il file
-            my_file = open(self.name, 'r')
             range_lines = my_file.readlines()
-            # scorro le varie linee del file
-            for line in range_lines:
-                elements = line.split(',')
-                # salto intestazione
-                if 'date' in line:
-                    continue
-                # gestisco i doppioni delle date. con doppioni alzo errore
-                if elements[0] in lista_date:
-                    raise ExamException("ERRORE. Non si vuole duplicati di date")
-                else:
-                    lista_date.append(elements[0])
+        except Exception:
+            raise ExamException("Errore in apertura file")
+        for line in range_lines:
+            elements = line.split(',')
+            # salto intestazione
+            if 'date' in line:
+                continue
+            anno_mese = elements[0].strip()
+            try:
+                anno_mese = datetime.datetime.strptime(anno_mese, "%Y-%m")
+                liste_timestamp.append(anno_mese)
+            except Exception:
+                continue
 
-                # INSERIRE CONTROLLO ORDINE CRESCENTE SU TIMESTAMP
-                # provo a convertire il valore dei passeggeri ad intero,
-                # se non è possibile gestisco errore
-                try:
-                    elements[-1] = int(elements[-1].strip())
-                except:
-                    print("Numero non convertibile a intero. Trasformato  in 0")
-                    elements[-1] = 0
-                if elements[-1] < 0:
-                    print("Numero negativo non accettato. Trasformato in 0")
-                    elements[-1] = 0
-                # inserisco i valori nelle due liste
-                passengers.append(elements[-1])
-                info.append(elements)
-        # chiudo file
-        my_file.close()
-        return info
+            try:
+                elements[1] = int(elements[1].strip())
+                if elements[1] < 0:
+                    elements[1] = 0
+            except IndexError:
+                elements[1] = 0
+            
 
+            lista_finale.append(elements)
 
-# ====================================================================
-#           ISTANZIAMENTO CLASSE, CHIAMATA METODO, CHIAMATA FUNZIONE
-# ====================================================================
+        # controllo ordinamento timestamps
+        for x in range(len(liste_timestamp)-1):
+            if liste_timestamp[x] > liste_timestamp[x+1]:
+                raise ExamException("I valori temporali non sono in ordine cronologico")
 
+        # per ogni valore appartenente alla lista timestamp,
+        # verifico se ce ne sia uno identico che va dalla posizione successiva fino a fondo lista
+        for x in range(len(liste_timestamp) - 1):
+            for y in range(x + 1, len(liste_timestamp)):
+                if liste_timestamp[x] == liste_timestamp[y]:
+                    print("Ci sono dei valori temporali duplicati nel file")
 
-file_name = r"/Users/simoneperessini/Desktop/data.csv"
-time_series_file = CSVTimeSeriesFile(name=file_name)
-time_series = time_series_file.get_data()
-print(time_series)
+        return lista_finale
 
-# LISTE DELLE ANNATE SENZA DUPLICATI
-anni = []
-lista_anni = []
-# inserisco nella lista anni tutti gli anni trovati nella lista info
-for x in lista_date:
-    if x not in anni:
-        anni.append(x[0:4])
-
-# tengo solo gli anni univoci della lista anni
-for y in anni:
-    if y not in lista_anni:
-        lista_anni.append(y)
-
-# ====================================================================
-#           DEFINIZIONE FUNZIONE DI CALCOLO MEDIA PASSEGGERI
-# ====================================================================
-
+# ================================ #
+#  COMPUTE AVG MONTHLY DIFFERENCE  #
+# ================================ #
 def compute_avg_monthly_difference(time_series, first_year, last_year):
-    if (type(first_year) != str) & (type(last_year) != str):
-        print("I due estremi intervalli non sono entrambi stringhe")
-    elif (first_year not in lista_anni) | (last_year not in lista_anni):
-        print("Almeno uno dei due estremi non è un valore contenuto nel file")
-    elif (int(first_year) > int(last_year)):
-        print("Estremo iniziale intervallo è superiore a estremo finale")
+    lista_anni = []
+    incremento_pass = []
+    for x in time_series:
+        lista_anni.append(x[0][0:4])
+
+    # Check if time_series is a list
+    if not isinstance(time_series, list):
+        raise ExamException('ERRORE, time series non è una lista di liste.')
     else:
-        incremento_pass = []
-        indice_inizio = anni.index(first_year)
+        if isinstance(time_series, list):
+            if not isinstance(time_series[0], list):
+                raise ExamException('ERRORE, time series non è una lista di liste.')
+
+    if (type(first_year) != str) & (type(last_year) != str):
+        raise ExamException("I due estremi intervalli non sono entrambi stringhe")
+    elif (first_year not in lista_anni) | (last_year not in lista_anni):
+        raise ExamException("Almeno uno dei due estremi non è un valore contenuto nel file")
+    elif (int(first_year) > int(last_year)):
+        raise ExamException("Estremo iniziale intervallo è superiore a estremo finale")
+    else:
+        # incremento_pass = []
+        indice_inizio = lista_anni.index(first_year)
         # dal momento voglio incluso anche estremo finale
         # converto in numero last_year, aggiungo 1 per avere posizione corretta
         # lo ripasso a stringa e poi ad index che mi restituisce posizione
-        indice_fine = anni.index(str(int(last_year) + 1))
-        intervallo = info[indice_inizio: indice_fine]
-        delta_anni = int(last_year) - int(first_year)
 
+        indice_fine = lista_anni.index(str(int(last_year) + 1))
+        intervallo = time_series[indice_inizio: indice_fine]
+        delta_anni = int(last_year) - int(first_year)
         i = 0
         diff = 0
         if delta_anni == 0:
@@ -157,8 +143,6 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
                     incremento_pass.append(0)
                 else:
                     n_addizioni = 0
-                    # anno_attuale = n_addizioni * 12 + i
-                    # anno_succ = (n_addizioni + 1) * 12 + i
                     while n_addizioni < delta_anni:
                         media = (float(intervallo[(n_addizioni + 1) * 12 + i][1]) - float(
                             intervallo[n_addizioni * 12 + i][1])) / delta_anni
@@ -169,8 +153,9 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
                 i += 1
         return incremento_pass
 
-
-first_year = "1954"
-last_year = "1958"
-variazioni_passeggeri = compute_avg_monthly_difference(time_series, first_year, last_year)
-print(variazioni_passeggeri)
+# istanze della classe
+#file_name = r"/Users/simoneperessini/Desktop/data.csv"
+time_series_file = CSVTimeSeriesFile(name='data.csv')
+time_series = time_series_file.get_data()
+print(time_series)
+print(compute_avg_monthly_difference(time_series, '1949', '1951'))
